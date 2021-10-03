5f59f7c1743c563d40b44a239f5b469eceadbfa5
Japaneseonly <18e1003@andrew.ac.jp>
2021-10-04 01:12:07 +0900

    sakujo

--- Assets/EncountManager.cs
+++ Assets/EncountManager.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;//Text型を扱うために導入
+using UnityEngine.UI;
 public class EncountManager : MonoBehaviour
 {
-    [SerializeField] Text ResultText = null;//エンカウント処理結果を表示させるTextオブジェクトとの連携のために導入
-    int[] resultID = new int[20];//処理結果を表すID resultID[0]:スライムとのエンカウント　resultID[1]:ゴブリンとのエンカウント　resultID[2]:エンカウントなしを表すものとする
-    float[] resultAR = new float[20];//各resultIDの出現率(resutAR[resultID]がresultIDが示す事象の出現確率を示す）    
-    //エネミーとのエンカウント計算処理を行うメソッド
+    [SerializeField] Text ResultText = null;
+    int[] resultID = new int[20];
+    float[] resultAR = new float[20];    
+    
     public void EncountCulc()
     {
-        int result;//エンカウント計算結果のresultIDを格納する作業用変数
-        int count = 0;//作業用変数
-        int[] encount = new int[100];//各確率に応じた数のresultIDを格納する変数(今回の場合は出現率は整数であるとして場合は配列の大きさを100とする)
-        //変数の初期化処理///////////////////////////
+        int result;
+        int count = 0;
+        int[] encount = new int[100];
+       
         for (int i = 0; i < 20; i++)
         {
             resultID[i] = i;
         }
-        resultAR[0] = 0.08f;//スライムとのエンカウント率を30%に設定
-        resultAR[1] = 0.02f;//ゴブリンとのエンカウント率を20%に設定
+        resultAR[0] = 0.08f;
+        resultAR[1] = 0.02f;
         resultAR[2] = 0.03f;
         resultAR[3] = 0.07f;
         resultAR[4] = 0.06f;
@@ -39,32 +39,30 @@ public class EncountManager : MonoBehaviour
         resultAR[18] = 0.05f;
         resultAR[19] = 0.04f;
 
-        //魔物と遭遇しない確率を50%に設定
-        //変数の初期化処理終了////////////////////////
-        //encount配列の中身を0に初期化
+        
         for (int i = 0; i < encount.Length; i++)
         {
             encount[i] = 0;
         }
-        //resultIDの出現率に従ってresultIDを配列変数encount格納する処理
+        
         for(int i = 0; i < resultID.Length; i++)
         {
             if (resultAR[i] != 0)
             {
-                for (int j = 0; j < Mathf.Floor(resultAR[i] * 100); j++)//出現率がfloatなので100倍して整数にする。小数点以下の誤差対策として切り捨て処理(Mathf.Floor)を使用する
+                for (int j = 0; j < Mathf.Floor(resultAR[i] * 100); j++)
                 {
                     encount[count] = resultID[i];
                     count++;
                 }
             }
         }
-        //ランダム関数を使用するので、先にランダムシードを時刻により初期化して、ランダム性を上げる
+        
         Random.InitState(System.DateTime.Now.Millisecond);
-        //ランダムインデックスを計算
+        
         int randomIndex = Random.Range(0, encount.Length);
-        //確率に応じたにエンカウント計算結果を変数result
+        
         result= encount[randomIndex];
-        //計算結果に応じてテキストオブジェクトの表示を変更する処理
+       
         if (result == resultID[0])
         {
             ResultText.text = "変異ウイルスの出現";
